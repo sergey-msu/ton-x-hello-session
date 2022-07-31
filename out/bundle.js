5b7c5fdc4de8dc4fc6870abf71b1be8c8e996fb2
sergei.polianskikh <sergei.polianskikh@plarium.com>
2022-07-31 09:34:08 +0300

    mobile support

--- out/bundle.js
+++ out/bundle.js
@@ -66311,7 +66311,6 @@ function sendCoins(to, amount, message, sessionSeed, appPublicKey, timeoutMin = 
             value: amount.toString(),
             timeout: timeoutMin * 60 * 1000,
             text: message,
-            payload: "te6cckEBAQEAUgAAn1/MPRQAAAAAAAADCYAKPvFPCYEV50Xs1IxFEXymxw/6eFx+moz4Nq7/qBZlv/AART9XH37phxxDoNGmcKc7t9AS4iR76HPj2Oj0dgSY82wI8rcSpA=="
         };
         const response = yield connector.requestTransaction(request);
         console.log(response);
@@ -66349,12 +66348,31 @@ var utils_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _ar
     });
 };
 
-function showQRCode(text, qrCanvas) {
+function mobileAndTabletCheck() {
+    let check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
+        check = true; })(navigator.userAgent || navigator.vendor);
+    console.log("Device is mobile: " + check.toString());
+    return check;
+}
+;
+function showQRCode(link, resDiv) {
     return utils_awaiter(this, void 0, void 0, function* () {
-        browser["toCanvas"](qrCanvas, text, function (error) {
+        const qrCanvas = document.createElement("canvas");
+        resDiv.appendChild(qrCanvas);
+        browser["toCanvas"](qrCanvas, link, function (error) {
             if (error)
                 console.error(error);
         });
+    });
+}
+function showQRButton(link, resDiv) {
+    return utils_awaiter(this, void 0, void 0, function* () {
+        const qrButton = document.createElement("button");
+        qrButton.setAttribute("class", "btn btn-info btn-lg px-4 gap-3");
+        qrButton.setAttribute("value", "Link");
+        qrButton.setAttribute("onclick", link);
+        resDiv.appendChild(qrButton);
     });
 }
 function showConnectResult(session, fromDiv, resDiv) {
@@ -66388,8 +66406,8 @@ var scripts_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _
 
 const connectBtn = document.getElementById("ts-cap-btn");
 const payBtn = document.getElementById("ts-pay-btn");
-const qrCancas = document.getElementById("ts-qr-canvas");
-const resDiv = document.getElementById("ts-res-div");
+const scripts_resDiv = document.getElementById("ts-res-div");
+const linkDiv = document.getElementById("ts-link-div");
 const fromDiv = document.getElementById("ts-from-div");
 const recipientInput = document.getElementById("ts-to-input");
 let sessionId = "";
@@ -66401,9 +66419,15 @@ connectBtn.addEventListener("click", (e) => scripts_awaiter(void 0, void 0, void
     const session = yield getSession();
     sessionId = session.id;
     sessionSeed = session.seed;
-    yield showQRCode(session.link, qrCancas);
+    const isMobile = mobileAndTabletCheck();
+    if (isMobile) {
+        yield showQRButton(session.link, linkDiv);
+    }
+    else {
+        yield showQRCode(session.link, linkDiv);
+    }
     const result = yield waitSession(session.id);
-    const wallet = yield showConnectResult(result, fromDiv, resDiv);
+    const wallet = yield showConnectResult(result, fromDiv, scripts_resDiv);
     if (wallet) {
         walletConfig = wallet.walletConfig;
         appPublicKey = wallet.appPublicKey;
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,12 +1,12 @@
 import { TonhubCreatedSession, TonhubSessionAwaited, TonhubWalletConfig } from "ton-x";
 import { getSession, waitSession, sendCoins } from './tonhub';
-import { showQRCode, showConnectResult } from "./utils";
+import { mobileAndTabletCheck, showQRCode, showQRButton, showConnectResult } from "./utils";
 
 
 const connectBtn: HTMLElement | any = document.getElementById("ts-cap-btn")
 const payBtn: HTMLElement | any = document.getElementById("ts-pay-btn")
-const qrCancas: HTMLElement | any = document.getElementById("ts-qr-canvas");
 const resDiv: HTMLElement | any = document.getElementById("ts-res-div");
+const linkDiv: HTMLElement | any = document.getElementById("ts-link-div");
 const fromDiv: HTMLElement | any = document.getElementById("ts-from-div");
 const recipientInput: HTMLElement | any = document.getElementById("ts-to-input");
 
@@ -15,13 +15,19 @@ let sessionSeed: string = "";
 let walletConfig: string = "";
 let appPublicKey: string = "";
 
+
 connectBtn.addEventListener("click", async (e:any) => {
   e.preventDefault();
 
   const session: TonhubCreatedSession = await getSession();
   sessionId = session.id;
   sessionSeed = session.seed;
-  await showQRCode(session.link, qrCancas);
+  const isMobile = mobileAndTabletCheck();
+  if (isMobile) {
+    await showQRButton(session.link, linkDiv);
+  } else {
+    await showQRCode(session.link, linkDiv);
+  }
 
   const result: TonhubSessionAwaited = await waitSession(session.id);
   const wallet: TonhubWalletConfig | null = await showConnectResult(result, fromDiv, resDiv);
